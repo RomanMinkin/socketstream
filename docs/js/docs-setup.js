322e78c63cc7a7e87a9fb007eeee643b890b1ce4
Roman Minkin <rminkin@vaynermedia.com>
2014-01-15 12:58:44 -0500

    Docs site diles have been updates

---
+++ docs/js/docs-setup.js
@@ -0,0 +1,100 @@
+NG_DOCS={
+  "sections": {
+    "tutorials": "Tutorials",
+    "api": "API Documentation",
+    "demos": "Demos"
+  },
+  "pages": [
+    {
+      "section": "tutorials",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "Guide to SocketStream Documentation",
+      "keywords": "documentation guide overview socketstream tutorials"
+    },
+    {
+      "section": "tutorials",
+      "id": "authentication",
+      "shortName": "Authentication",
+      "type": "overview",
+      "moduleName": "Authentication",
+      "shortDescription": "Authentication",
+      "keywords": "access accesstoken accesstokensecret action actions add adding app append apps authenticate authenticated authenticates authenticating authentication backend best bodyparser browser callback calling change choice client closely code coming command connect connection console consumer consumerkey consumersecret contents control css current data database db define denied details docs don easily enter events everyauth example examples exports express facebook findorcreateuser follow full function geared getcurrentuser github goal host http https integrates internal jade john js ldap libraries libs listen ll load log logout lookup main manually middleware modules multi-page note notifies oauth object option output overview password popular prepend processing production publish real redirectpath register req request require res resource return route router rpc save saves screen_name second secret sending sends serve server service session set setuserid simple socketstream specific ss start started styl supply tempted testing third-party true tutorials twitter twitterusermetadata unique update updating url user userid username users var view visit ways website websocket websockets whilst"
+    },
+    {
+      "section": "tutorials",
+      "id": "client_side_code",
+      "shortName": "Client-Side Code",
+      "type": "overview",
+      "moduleName": "Client-Side Code",
+      "shortDescription": "Client-Side Code",
+      "keywords": "ability absolutely access accessed add allowing allows alphanumerically amd amount app approaches as-is automatically aware awesome backbone background bear better boiler-plate brought browser browserify browserifyexcludepaths cache called calls case cases catch chaos choice clean client client-code client-side code coffee coming command common components connection console copy create created critical critically default define demand depend dependencies depends developer developers difference directores directory disable distinction doesn don easily ensure entry error established exactly example exceptions exclude excluded excluding executed exist expect experience explicitly export file files folder forget form functions future goal going great handle head history hopes https inbuilt info instantly internally javascript jquery js killing leading legacy libraries library libs lightweight list live lives ll load loading long magic major making manage managing manually md mess mind modification modify module modules namespacing node onus options order org overview packaging path paths performs point position problem problems project querystring reconnecting reference regular relative require required requires send serve served server server-side set share sharing side single slash small socketstream solution solutions solves special ss stack statement structure subdirectories substack syntax system top track treated tricky tutorials type typically underscore unique unstructured url values variable wade wanted web websocket window work works write writing"
+    },
+    {
+      "section": "api",
+      "id": "http.index:Server",
+      "shortName": "Server",
+      "type": "overview",
+      "moduleName": "http",
+      "shortDescription": "HTTP Server",
+      "keywords": "api application concern connect http middleware overview server servers simply socketstream stack web wishes"
+    },
+    {
+      "section": "api",
+      "id": "http.router:Router",
+      "shortName": "Router",
+      "type": "overview",
+      "moduleName": "http",
+      "shortDescription": "Right now the router is simply an EventEmitter. This may change in the future",
+      "keywords": "api change eventemitter future http overview router simply"
+    },
+    {
+      "section": "api",
+      "id": "utils.file:findExtForBasePath",
+      "shortName": "findExtForBasePath",
+      "type": "overview",
+      "moduleName": "utils",
+      "shortDescription": "Given a basename, find a matching file with an extension.",
+      "keywords": "api array basename basepath css directory extension file files find findextforbase jade matching null overview path utils views"
+    },
+    {
+      "section": "api",
+      "id": "utils.file:loadPackageJSON",
+      "shortName": "loadPackageJSON",
+      "type": "overview",
+      "moduleName": "utils",
+      "shortDescription": "Loads SocketStream&#39;s package.json file from the root directory",
+      "keywords": "api directory file json loads object overview package root socketstream utils"
+    },
+    {
+      "section": "api",
+      "id": "utils.file:readDirSync",
+      "shortName": "readDirSync",
+      "type": "overview",
+      "moduleName": "utils",
+      "shortDescription": "Reads the contents of a dir. Adapted from https://gist.github.com/825583",
+      "keywords": "adapted api contents dir directory file github https lookup overview reads start utils"
+    },
+    {
+      "section": "demos",
+      "id": "index",
+      "shortName": "index",
+      "type": "overview",
+      "moduleName": "index",
+      "shortDescription": "Demos",
+      "keywords": "demos overview"
+    }
+  ],
+  "apis": {
+    "tutorials": false,
+    "api": true,
+    "demos": false
+  },
+  "html5Mode": false,
+  "startPage": "/api",
+  "scripts": [
+    "angular.min.js"
+  ]
+};
